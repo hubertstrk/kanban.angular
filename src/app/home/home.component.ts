ae0c70d6b048f6be6fdf9917d2812d0d84aa1aa0
Hubert Stärk <hubert.staerk@gmail.com>
2025-06-15 00:50:35 +0200

    Integrate reusable button component and SVG icon service for streamlined window management actions. Remove obsolete `tsconfig.spec.json`.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {getCurrentWebviewWindow} from '@tauri-apps/api/webviewWindow';
+import {Subject, takeUntil} from 'rxjs';
+import {IconService} from "@services/icons.service";
+import {SafeHtml} from '@angular/platform-browser';
+
+import {ButtonComponent} from '../shared/button/app-button.component';
 
 const appWindow = getCurrentWebviewWindow();
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
+  providers: [IconService],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
-  title = 'Kanban Board';
+export class HomeComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+
+  icons: { [key: string]: SafeHtml } = {};
+
+  constructor(
+    private iconService: IconService
+  ) {
+  }
+
+  ngOnInit() {
+    this.iconService
+      .getIcons([
+        'material-symbols-light--minimize-rounded',
+        'material-symbols-light--square-outline-rounded',
+        'material-symbols-light--close-rounded',
+      ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(icons => {
+        this.icons = icons;
+      });
+  }
 
   minimizeWindow() {
     void appWindow.minimize();
@@ -23,5 +49,10 @@ export class HomeComponent {
 
   closeWindow() {
     void appWindow.close();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
